04923a57dca32131309df86cc4ab67111f4f3993
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2024-09-26 06:00:53 +0530

    some update

--- index.js
+++ index.js
@@ -10,9 +10,41 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   fs.readdir(`./files`, (err, files) => {
-    console.log(files);
+    res.render("index", { files: files });
   });
-  res.render("index");
+});
+
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", {
+      filename: req.params.filename,
+      filedata: filedata,
+    });
+  });
+});
+
+app.get("/edit/:filename", (req, res) => {
+  res.render("edit", { filename: req.params.filename });
+});
+
+app.post("/edit", (req, res) => {
+  fs.rename(
+    `./files/${req.body.previous}`,
+    `./files/${req.body.new}`,
+    (err) => {
+      res.redirect("/");
+    }
+  );
+});
+
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.description,
+    (err) => {
+      res.redirect("/");
+    }
+  );
 });
 
 app.listen(3000);
